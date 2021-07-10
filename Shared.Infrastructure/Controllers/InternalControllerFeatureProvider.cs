6d38fb99e4101b80f94a0d999f4c39df4fdb97ea
Mukesh Murugan <iammukeshm@gmail.com>
2021-07-10 10:55:52 +0530

    added controller

---
+++ Shared.Infrastructure/Controllers/InternalControllerFeatureProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Infrastructure.Controllers
+{
+internal class InternalControllerFeatureProvider : ControllerFeatureProvider
+{
+    protected override bool IsController(TypeInfo typeInfo)
+    {
+        if (!typeInfo.IsClass)
+        {
+            return false;
+        }
+        if (typeInfo.IsAbstract)
+        {
+            return false;
+        }
+        if (typeInfo.ContainsGenericParameters)
+        {
+            return false;
+        }
+        if (typeInfo.IsDefined(typeof(NonControllerAttribute)))
+        {
+            return false;
+        }
+        return typeInfo.Name.EndsWith("Controller", StringComparison.OrdinalIgnoreCase) ||
+                typeInfo.IsDefined(typeof(ControllerAttribute));
+    }
+}
+}
---
+++ Shared.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Infrastructure.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Infrastructure.Extensions
+{
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSharedInfrastructure(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddControllers()
+            .ConfigureApplicationPartManager(manager =>
+            {
+                manager.FeatureProviders.Add(new InternalControllerFeatureProvider());
+            });
+        return services;
+    }
+}
+}
